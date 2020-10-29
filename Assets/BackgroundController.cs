f1e54b3aa8b6c55d22a60459bd3d2e7340255db6
mitsuru_yamaura <komayou326@gmail.com>
2020-10-29 16:31:28 +0900

    アセット追加２。

---
+++ Assets/BackgroundController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    public float scrollSpeed = 0.01f;
+
+    public float deadLine = -16f;
+
+    public float startLine = 15.8f;
+
+    void Start()
+    {
+        startLine = transform.position.x;
+        Debug.Log(startLine);
+    }
+
+    void Update() {
+        
+        // 画面の左方向に背景を移動する
+        transform.Translate(-scrollSpeed, 0, 0);
+
+        if (transform.position.x < deadLine) {
+            transform.position = new Vector2(startLine, 0);
+        }
+    }
+}
