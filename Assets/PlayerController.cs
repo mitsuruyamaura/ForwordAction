522f7f8b2181cec9e269c46027e4bb976b735cc9
mitsuru_yamaura <komayou326@gmail.com>
2020-10-29 17:11:51 +0900

    Player設置。左右移動、空中浮遊処理実装。

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private string horizontal = "Horizontal";
+    private string jump = "Jump";
+
+    private Rigidbody2D rb;
+
+    public float jumpPower;
+    public float moveSpeed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ジャンプ
+        if (Input.GetButtonDown(jump)) {
+            rb.AddForce(transform.up * jumpPower);
+        }
+
+        // 移動
+
+        float posX = Input.GetAxis(horizontal);
+
+        rb.velocity = new Vector2(posX * moveSpeed, rb.velocity.y);
+
+        // 移動方向に向きを合わせる
+        if (posX > 0) {
+            transform.localScale = new Vector3(1, 1, 1);
+        } else {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        
+    }
+}
