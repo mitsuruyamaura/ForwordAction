b9ecd33771e3d68a70d2dbeed2921e8d3e90ed70
mitsuru_yamaura <komayou326@gmail.com>
2020-10-30 14:51:06 +0900

    敵とコインと地面のランダム生成処理実装。DoTween追加。

---
+++ Assets/MoveObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    public float moveSpeed;
+
+    void Update() {
+        transform.position += new Vector3(-moveSpeed, 0, 0);
+    }
+}
